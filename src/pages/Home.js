412a9b88ca0a323b0bf320182652cb7971e54280
ullah <sheikholeei97@gmail.ocm>
2022-10-26 19:16:59 +0600

    home page design

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,22 @@
 import React from 'react'
-import bg from '../assets/images/bg.jpg'
+import bg from '../assets/images/bg.jpg';
+import wing from '../assets/images/wing.png';
+
 
 
 const Home = () => {
 
   return (
-    <div style={{backgroundImage: `url(${bg})`}} className='bg-fixed bg-cover bg-center h-[92vh] w-full bg-[#13131388] bg-blend-overlay overflow-hidden'>
-        <h1 className='text-center text-5xl font-bold'>THis is the HOmepage</h1>
+    <div style={{backgroundImage: `url(${bg})`}} className='bg-fixed bg-cover bg-center w-full bg-[#13131388] bg-blend-overlay overflow-hidden'>
+        <div className='grid md:grid-cols-2 py-10 gap-5'>
+          <div className='flex items-center justify-center '>
+              <h1 className='text-3xl md:text-5xl lg:text-6xl leading-20 italic font-extrabold text-white uppercase '>A New different<br/> Way to improve <br/> Your skills</h1>
+          </div>
+          <div>
+            <img src={wing} alt="" />
+          </div>
+        </div>
+
     </div>
   )
 }
